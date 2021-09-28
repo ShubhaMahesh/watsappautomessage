2326125587d549d42be668a306318bf91726fff4
Shubha Mahesh <88620180+ShubhaMahesh@users.noreply.github.com>
2021-09-28 12:17:45 +0530

    Initial commit

---
+++ watsappautomation.py
@@ -0,0 +1,5 @@
+#open pycharm then enter the code
+import pywhatkit
+pywhatkit.sendwhatmsg('+91123456789','hello Happy birthday', 11, 39)
+#run the code
+#in some sec watsapp web gets started thn msg will be sent to the person
